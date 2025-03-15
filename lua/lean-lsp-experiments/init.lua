83daa5a89b42c42feb125081052baa112a57468f
Patrick Massot <patrickmassot@free.fr>
2025-03-15 18:03:27 +0100

    Try binding function from edit.lua

--- lua/lean-lsp-experiments/init.lua
+++ lua/lean-lsp-experiments/init.lua
@@ -17,6 +17,16 @@ local function hello()
   print("Hello")
 end
 
+vim.keymap.set('n', '[m', edit.declaration.goto_start, {
+  buffer = true,
+  desc = 'Move to the previous declaration start.',
+})
+
+vim.keymap.set('n', ']m', edit.declaration.goto_end, {
+  buffer = true,
+  desc = 'Move to the next declaration end.',
+})
+
 LeanLspExperiments.hello = hello
 LeanLspExperiments.declarationRangeAt = declarationRangeAt
 
